51f6b9a0fdd642bc2b45adfa7e921a604113e269
RonmelT123 <a2019210835@estudiantes.upsa.edu.bo>
2021-10-13 19:45:10 -0400

    Agregar archivos de proyecto.

---
+++ Models/Suerte.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial_2_programacion.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public string NombreID { get; set; }
+        public string Detalle { get; set; }
+        public string Imagen { get; set; }
+    }
+}
